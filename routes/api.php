17ac8cafa06d89bd0ffc97aa550e3bfea4d09b46
Jean Freitas <henriquek3@live.com>
2021-02-23 11:27:57 -0400

    sanctum

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/test', function (Request $request) {
+    return response()->json(
+        [
+            'data' => 'testado com sucesso!'
+        ]
+    );
 });
